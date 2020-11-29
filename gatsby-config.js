00eafd67a891ba5f5fe8074d067e8a467938eb00
FlorSanders <florsanders@gmail.com>
2020-11-29 20:30:13 +0100

    Added some plugins I will need

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,56 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+    siteMetadata: {
+        title: 'Flor Sanders',
+        description: 'Personal and business website for Flor Sanders.\nA driven engineering student and starting entrepreneur.',
+        keywords: 'Student, Entrepreneur, Developer, Software, Hardware, Open Data, IoT, Guitar, Travel, Cooking, Business, Personal',
+        author: 'Flor Sanders',
+    },
+    plugins: [
+        // Transformer plugins needed for Gatsby image
+        `gatsby-transformer-sharp`, `gatsby-plugin-sharp`,
+        // Gatsby Image
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: path.join(__dirname, `src`, `images`),
+            },
+        },
+        // Styling: Tailwind CSS
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                postCssPlugins: [
+                    require("tailwindcss"),
+                    require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+                ],
+            },
+        },
+        // Source from file system plugin
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images/`,
+            },
+        },
+        // SEO Manager
+        `gatsby-plugin-react-helmet`,
+        // Make The site work offline
+        `gatsby-plugin-offline`,
+        // Automatically create a sitemap
+        `gatsby-plugin-sitemap`,
+        // Google analytics
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+                head: false,
+                anonymize: true,
+            },
+        },
+    ],
 }
 c
