ed136be2e9ecde9cd50ad30227f2181059a84a07
FlorSanders <florsanders@gmail.com>
2020-12-16 22:42:25 +0100

    Changed 404 page title

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import { Layout } from '../components';
 export default function NotFound({data}) {
     var image = data.file.childImageSharp.fluid;
     return (
-        <Layout title='Error 404: Page Not Found' image={image} fullheight={true}>
+        <Layout title='Nothing to see here...' image={image} fullheight={true}>
         </Layout>
     )
 }
