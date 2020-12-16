e272a8ded5d228e85bb6cda5a66aa02519ed6963
FlorSanders <florsanders@gmail.com>
2020-12-16 22:24:17 +0100

    Hamburger menu is now fully functional as well

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'gatsby';
-import { pages } from '../constants';
+import { pages, breakPoints } from '../constants';
 
 export default function Navbar() {
+    // Deals with opening and closing of the mobile hamburger menu
+    var [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = (e) => {
+        e.preventDefault();
+        setMenuOpen(!menuOpen);
+    }
+
+    // Closes the hamburger menu if we resize the window and it gets larger than mobile
+    useEffect(() => {
+        function handleResize() {
+            if(window.innerWidth > breakPoints.mobile){
+                setMenuOpen(false);
+            }
+        }
+        window.addEventListener('resize', handleResize);
+    })
+
     return (
         <div className='container is-max-widescreen'>
             <div className='navbar is-transparent m-5'>
@@ -10,6 +27,22 @@ export default function Navbar() {
                     <Link to='/' className='navbar-item has-text-light title is-4'>
                         Flor Sanders
                     </Link>
+                    
+                    <div className={`dropdown dropdown-navbar is-right${menuOpen?' is-active':''}`}>
+                        <a class={`navbar-burger burger has-text-light${menuOpen?' is-active':''}`} href='/' onClick={toggleMenu}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </a>
+
+                        <div className="dropdown-menu">
+                            <div className='navbar-items'>
+                                {pages.map((page) => (
+                                    <Link key={page} to={`/${page.toLowerCase()}`} className='navbar-item has-text-light is-size-4 is-justify-content-end'>{page}</Link>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="navbar-menu">
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,12 @@
 /* Constants used in multiple parts of the site */
 export const pages = ['About', 'Services', 'Blog', 'Contact'];
 export const whatIAm = ['n engineering student', ' freelance developer', ' maker', 'n IT consultant', ' blogger', ' human'];
+export const breakPoints = {
+    mobile: 768,
+    tablet: 1024,
+    desktop: 1216,
+    widescreen: 1408,
+}
+
+
+
