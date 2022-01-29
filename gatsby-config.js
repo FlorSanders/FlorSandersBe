c07f5fab97534a5527a484ed0663e6e4ee00aef8
Flor Sanders <me@florsanders.be>
2022-01-29 17:47:24 +0100

    Install tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,9 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         theme_color: `#000000`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-postcss`,
   ],
 }
