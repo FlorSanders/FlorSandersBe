7b3728cdba05b82c6dce4739c8b53929cf2feabb
Flor Sanders <me@florsanders.be>
2022-03-18 13:31:25 +0100

    Add rdf to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,65 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import { graphql } from "gatsby"
 
-import { NavigationTabs, TypingAnimation, HeroImage, SEO } from '../components';
-import { whatIAm } from '../constants';
+import { NavigationTabs, TypingAnimation, HeroImage, SEO } from "../components"
+import { whatIAm } from "../constants"
 
-import '../styles/index.scss';
+import "../styles/index.scss"
 
 export default function Home({ data }) {
-    var images = data.allImageSharp.edges.filter((elem) => (elem.node.parent.name.includes('main_background')));
-    var image = images[Math.floor(Math.random() * images.length)].node.fluid;
+  var images = data.allImageSharp.edges.filter(elem =>
+    elem.node.parent.name.includes("main_background")
+  )
+  var image = images[Math.floor(Math.random() * images.length)].node.fluid
 
-    return (
-        <React.Fragment>
-            <SEO title='Home' />
-            <HeroImage
-                image={image}
-                header={null}
-                footer={<NavigationTabs light={true}/>}
-                fullheight={true}
-            >
-                <div className='container has-text-centered'>
-                    <div className="subtitle is-3 has-text-light">
-                        Hi! My name is
-                    </div>
-                    <div className='title is-1 has-text-light'>
-                        Flor Sanders
-                    </div>
-                    <div className="subtitle is-3 has-text-light is-flex-direction-row">
-                        <TypingAnimation
-                            base={'I am a'}
-                            content={whatIAm}
-                        />
-                    </div>
-                </div>
-            </HeroImage>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <script type="text/turtle">
+        {`
+                @base <https://www.florsanders.be/>.
+                @prefix foaf: <http://xmlns.com/foaf/0.1/>.
+                @prefix schema: <http://schema.org/>.
+                <#me> 
+                    a foaf:Person;
+                    a schema:Person;
+                    foaf:name "Flor Sanders";
+                    foaf:knows <https://pietercolpaert.be/#me>.
+                `}
+      </script>
+      <SEO title="Home" />
+      <HeroImage
+        image={image}
+        header={null}
+        footer={<NavigationTabs light={true} />}
+        fullheight={true}
+      >
+        <div className="container has-text-centered">
+          <div className="subtitle is-3 has-text-light">Hi! My name is</div>
+          <div className="title is-1 has-text-light">Flor Sanders</div>
+          <div className="subtitle is-3 has-text-light is-flex-direction-row">
+            <TypingAnimation base={"I am a"} content={whatIAm} />
+          </div>
+        </div>
+      </HeroImage>
+    </React.Fragment>
+  )
 }
 
 export const query = graphql`
-    query {
-        allImageSharp {
-            edges {
-                node {
-                    fluid(quality: 100, maxWidth: 1920) {
-                        ...GatsbyImageSharpFluid_withWebp
-                    }
-                    parent {
-                        ... on File {
-                            name
-                        }
-                    }
-                }
+  query {
+    allImageSharp {
+      edges {
+        node {
+          fluid(quality: 100, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+          parent {
+            ... on File {
+              name
             }
+          }
         }
+      }
     }
+  }
 `
