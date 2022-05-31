449c8822b22cf0874ed86629f20ed8dcbfc7f364
Flor Sanders <me@florsanders.be>
2022-05-31 14:47:00 +0200

    Fresh start

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import { Layout } from '../components';
+import * as React from "react"
+import { Link } from "gatsby"
 
-export default function NotFound({data}) {
-    var image = data.file.childImageSharp.fluid;
-    return (
-        <Layout title='Nothing to see here...' image={image} fullheight={true}>
-        </Layout>
-    )
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: "96px",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
 }
 
-export const query = graphql`
-    query {
-        file(name: { eq: "404_background" }) {
-            childImageSharp {
-                fluid(quality: 100, maxWidth: 1920) {
-                    ...GatsbyImageSharpFluid_withWebp
-                }
-            }
-        }
-    }
-`
+const paragraphStyles = {
+  marginBottom: 48,
+}
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
+
+// markup
+const NotFoundPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Not found</title>
+      <h1 style={headingStyles}>Page not found</h1>
+      <p style={paragraphStyles}>
+        Sorry{" "}
+        <span role="img" aria-label="Pensive emoji">
+          😔
+        </span>{" "}
+        we couldn’t find what you were looking for.
+        <br />
+        {process.env.NODE_ENV === "development" ? (
+          <>
+            <br />
+            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            <br />
+          </>
+        ) : null}
+        <br />
+        <Link to="/">Go home</Link>.
+      </p>
+    </main>
+  )
+}
+
+export default NotFoundPage
