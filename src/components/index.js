4c9d4cd34b8153249763c7b6e0ffccfec13c4c41
Flor Sanders <me@florsanders.be>
2022-06-10 20:27:07 +0200

    Collect components

---
+++ src/components/index.js
@@ -0,0 +1,43 @@
+import P from "./paragraph";
+import A from "./link";
+import Ol from "./ordered-list";
+import Ul from "./unordered-list";
+import Li from "./list-element";
+import Slider from "./slider";
+import Navlink from "./navlink";
+import Section from "./section";
+import HorizontalSection from "./horizontal-section";
+import HorizontalContent from "./horizontal-section-content";
+import ColumnSection from "./column-section";
+import SectionColumn from "./column-section-column";
+import SliderSection from "./slider-section";
+import SectionSlide from "./slider-section-slide";
+import Header from "./header";
+import Footer from "./footer";
+import HomeNavbar from "./home-navbar";
+import Navbar from "./navbar";
+import Layout from "./layout";
+import Seo from "./seo";
+
+export {
+  P,
+  A,
+  Ol,
+  Ul,
+  Li,
+  Header,
+  Footer,
+  HomeNavbar,
+  Navbar,
+  Navlink,
+  Section,
+  HorizontalSection,
+  HorizontalContent,
+  ColumnSection,
+  SectionColumn,
+  Slider,
+  SliderSection,
+  SectionSlide,
+  Layout,
+  Seo,
+};
