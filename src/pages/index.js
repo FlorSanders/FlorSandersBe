b7371922144dbea89695fd5e51dc804b52011b73
Flor Sanders <me@florsanders.be>
2022-06-10 20:27:35 +0200

    About page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,261 @@
-import * as React from "react"
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import * as Icons from "react-icons/io5";
+import {
+  P,
+  A,
+  Ul,
+  Ol,
+  Li,
+  HorizontalSection,
+  ColumnSection,
+  SectionColumn,
+  HorizontalContent,
+  SliderSection,
+  SectionSlide,
+  Layout,
+} from "../components";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
-const IndexPage = () => {
+export default function About() {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+    <Layout
+      page="about"
+      title="About"
+      image={
+        <StaticImage
+          src="../assets/images/about-cover.jpg"
+          className="w-full brightness-75"
+          height={680}
+          quality={90}
+          alt="About cover image"
+        />
+      }
+    >
+      <aside className="w-full space-y-2 text-center text-xl sm:text-2xl md:text-3xl font-poppins font-thin pt-16 pb-12">
+        <p>MSc. EE Student at Ghent University</p>
+        <p>Freelance Developer at FS Technologies</p>
+      </aside>
+      <div className="flex flex-col w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12">
+        <HorizontalSection
+          title="Who am I?"
+          image={
+            <StaticImage
+              src="../assets/images/profile-picture.jpg"
+              quality={90}
+              className="w-80 rounded-md my-4"
+              alt="Portrait of Flor Sanders"
+            />
+          }
+        >
+          <P>Hi! My name is Flor Sanders.</P>
+          <P>
+            Ever since I was young, I've had a fascination with science and
+            technology. Over the years I have explored this interest, building
+            knowledge and honing my skills. Today, I make use innovative
+            approaches to solve real-world practical problems.
+          </P>
+          <P>
+            Besides engineering I enjoy cooking, play the guitar, like reading
+            and have a general interest in a broad range of other subjects, from
+            economy and entrepreneurship to psychology and philosophy.
+          </P>
+          <P>
+            As I love meeting new people, I'm alsways up for a drink.
+            <br />
+            Feel free to <A to="/#contact">reach out</A>!
+          </P>
+        </HorizontalSection>
+        <ColumnSection
+          title="Goals"
+          className="grid-cols-1 sm:grid-cols-2 md:grid-cols-3"
+        >
+          <SectionColumn title="Time" icon={<Icons.IoTimeOutline />}>
+            <P>
+              I truly believe that time is our most valuable possession. By
+              considering this fact often, I hope to spend it in a way that
+              helps me reach all of my other goals.
+            </P>
+          </SectionColumn>
+          <SectionColumn title="Learning" icon={<Icons.IoBookOutline />}>
+            <P>
+              I'm an infinitely curious person. Learning about all aspects about
+              the wonderful world we live in is what drives me.
+            </P>
+          </SectionColumn>
+          <SectionColumn title="Health" icon={<Icons.IoHeartOutline />}>
+            <P>Mens sana in corpore sano.</P>
+            <P>
+              Even the old Greek and Romans already knew the importance of
+              physical health.
+            </P>
+          </SectionColumn>
 
-export default IndexPage
+          <SectionColumn title="Relationships" icon={<Icons.IoPeopleOutline />}>
+            <P>
+              Building and maintaining deep, trustful relationships makes me
+              very happy. Hence, I spend a lot of time and effort in being a
+              supportive and present friend and family member.
+            </P>
+          </SectionColumn>
+          <SectionColumn title="Food" icon={<Icons.IoNutritionOutline />}>
+            <P>
+              Ever since volunteering in the kitchens of{" "}
+              <A to="https://clipvakanties.be/">Clip Taalvakanties</A>, cooking
+              has been a passion of mine. Food contains a lot of culture and
+              identity and has the power to bring people together.
+            </P>
+          </SectionColumn>
+          <SectionColumn title="Travel" icon={<Icons.IoTrainOutline />}>
+            <P>
+              Travelling not only covers my interest of seeing new places and
+              having novel experiences, they also offer the perfect opportunity
+              to spend quality time with friends and family.
+            </P>
+          </SectionColumn>
+        </ColumnSection>
+        <SliderSection random={true} title="What I'm working on">
+          <SectionSlide
+            title="Master thesis"
+            image={
+              <StaticImage
+                src="../assets/images/thesis.jpg"
+                quality={90}
+                className="w-80 rounded-md my-4"
+                alt="Antenna Array"
+              />
+            }
+            flip={true}
+          >
+            <P>
+              During my master thesis at the{" "}
+              <A to="https://em.idlab.ugent.be/welcome.php">
+                EM research group
+              </A>{" "}
+              I research 140 GHz System-on-Chip (SoC) antenna arrays for
+              beyond-5G applications.
+            </P>
+            <P>
+              The thesis is planned to be finished by June 2023, which will also
+              mark the completion of my engineering studies.
+            </P>
+          </SectionSlide>
+          <div className="px-4">
+            <HorizontalContent
+              image={
+                <StaticImage
+                  src="../assets/images/fris.jpg"
+                  quality={90}
+                  className="w-80 rounded-md my-4"
+                  alt="Portrait of Flor Sanders"
+                />
+              }
+            >
+              <h3 className="text-2xl font-poppins text-center">
+                Student representation
+              </h3>
+              <P>
+                As a former board member at{" "}
+                <A to="https://fris.ugent.be/">FRiS</A> and member of the
+                faculty council of the{" "}
+                <A to="https://www.ugent.be/ea">UGent FEA</A> as well as various
+                commitees, I defend the interests of my fellow engineering
+                students.
+              </P>
+              <P>My main points of focus are:</P>
+              <Ol>
+                <Li>
+                  High quality courses and learning material, both online and in
+                  person.
+                </Li>
+                <Li>
+                  Stimulation of entrepreneurship and support for
+                  extracurricular activities.
+                </Li>
+                <Li>Support and encouragement for internationalization.</Li>
+              </Ol>
+            </HorizontalContent>
+          </div>
+          <div className="px-4">
+            <HorizontalContent
+              image={
+                <StaticImage
+                  src="../assets/images/freelance.jpg"
+                  quality={90}
+                  className="w-80 rounded-md my-4"
+                  alt="Portrait of Flor Sanders"
+                />
+              }
+              flip={true}
+            >
+              <h3 className="text-2xl font-poppins text-center">
+                Freelance projects
+              </h3>
+              <P>
+                As a freelance developer and IT consultant, I help organizations
+                solve problems using cutting-edge technologies.
+              </P>
+              <P>
+                My main strengths lie in web development and scientific
+                programming, though I'm not afraid to tackle other challenges.
+                I've had the chance of working for some amazing organizations,
+                such as:
+              </P>
+              <Ul>
+                <Li>
+                  <A to="https://redpencil.io/">Redpencil.io</A>
+                </Li>
+                <Li>
+                  <A to="https://www.wur.nl">
+                    Wageningen University & Research
+                  </A>
+                </Li>
+                <Li>
+                  <A to="https://alert-opleidingen.be/">Alert! Opleidingen</A>
+                </Li>
+              </Ul>
+              <P>
+                For more information, see my <A to="/projects">projects page</A>
+                .
+              </P>
+            </HorizontalContent>
+          </div>
+        </SliderSection>
+        <ColumnSection
+          id="contact"
+          title="Get in touch"
+          className="grid-cols-1 sm:grid-cols-2 md:grid-cols-3"
+        >
+          <SectionColumn title="Online" icon={<Icons.IoMailOutline />}>
+            <P>
+              For short questions or messages, tweet me{" "}
+              <A to="https://twitter.com/FlorPSanders">@FlorPSanders</A> or send
+              me a direct message on{" "}
+              <A to="https://www.linkedin.com/in/flor-sanders/">LinkedIn</A>.
+            </P>
+            <P>
+              For longer enquiries, please send me an{" "}
+              <A to="mailto:me@florsanders.be">email</A>.
+            </P>
+          </SectionColumn>
+          <SectionColumn title="Phone" icon={<Icons.IoCallOutline />}>
+            <P>
+              Want to discuss something quickly over the phone? Urgently need to
+              get a hold of me?
+            </P>
+            <P>
+              Feel free to give me a call on{" "}
+              <A to="tel:+32479873490">0479873490</A>.
+            </P>
+          </SectionColumn>
+          <SectionColumn title="Real life" icon={<Icons.IoCafeOutline />}>
+            <P>Real conversations happen in the real world.</P>
+            <P>
+              If you want to discuss something or just have a talk, hit me up
+              and we'll make it happen!
+            </P>
+          </SectionColumn>
+        </ColumnSection>
+      </div>
+    </Layout>
+  );
+}
