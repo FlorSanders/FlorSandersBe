98682b88773eca91d33a7dd867a67932c19bd117
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-06-12 09:07:18 -0700

    remove unused filesystem config and remove gatsby-node create types

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,60 +3,52 @@ module.exports = {
     title: `Flor Sanders`,
     siteUrl: `https://www.florsanders.be`,
     description:
-      "Hi! My name is Flor Sanders.\nI make use of cutting edge technologies to solve real-world practical problems.",
-    creator: "Flor Sanders (me@florsanders.be)",
+      'Hi! My name is Flor Sanders.\nI make use of cutting edge technologies to solve real-world practical problems.',
+    creator: 'Flor Sanders (me@florsanders.be)'
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-postcss",
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-postcss',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/assets/images/icon.png",
-      },
+        icon: 'src/assets/images/icon.png'
+      }
     },
     {
-      resolve: "gatsby-plugin-mdx",
+      resolve: 'gatsby-plugin-mdx',
       options: {
-        extensions: [".mdx", ".md"],
+        extensions: ['.mdx', '.md'],
         plugins: [
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 600,
-            },
-          },
-        ],
-      },
+              maxWidth: 600
+            }
+          }
+        ]
+      }
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/assets/images/",
+        name: 'images',
+        path: './src/assets/images/'
       },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "blog",
-        path: "./src/assets/blog/",
-      },
-      __key: "blog",
+      __key: 'images'
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "projects",
-        path: "./src/assets/projects/",
+        name: 'projects',
+        path: './src/assets/projects/'
       },
-      __key: "projects",
-    },
-  ],
+      __key: 'projects'
+    }
+  ]
 };
