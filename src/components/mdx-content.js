f9754cfc2832b6b8a2e20c78eb7797630b441761
Flor Sanders <me@florsanders.be>
2022-06-17 13:02:56 +0200

    Update MDX content components

--- src/components/mdx-content.js
+++ src/components/mdx-content.js
@@ -10,31 +10,46 @@ function MDXContent({ children, ...props }) {
       components={{
         p: (props) => <P {...props} />,
         /* eslint-disable jsx-a11y/heading-has-content */
-        h1: (props) => <h1 {...props} className="font-poppins text-6xl" />,
-        h2: (props) => <h2 {...props} className="font-poppins text-5xl" />,
-        h3: (props) => <h3 {...props} className="font-poppins text-4xl" />,
-        h4: (props) => <h4 {...props} className="font-poppins text-3xl" />,
-        h5: (props) => <h5 {...props} className="font-poppins text-2xl" />,
-        h6: (props) => <h6 {...props} className="font-poppins text-xl" />,
-        // blockquote: <blockqoute {...props} className="" />, // TODO
-        ul: (props) => <Ul {...props} />,
-        ol: (props) => <Ol {...props} />,
+        h1: (props) => <h1 {...props} className="font-poppins pt-2 text-4xl" />,
+        h2: (props) => <h2 {...props} className="font-poppins pt-2 text-3xl" />,
+        h3: (props) => <h3 {...props} className="font-poppins pt-2 text-2xl" />,
+        h4: (props) => <h4 {...props} className="font-poppins pt-2 text-xl" />,
+        h5: (props) => <h5 {...props} className="font-poppins pt-2 text-xl" />,
+        h6: (props) => <h6 {...props} className="font-poppins pt-2 text-xl" />,
+        blockquote: (props) => (
+          <div
+            {...props}
+            className="bg-gray-200 px-6 py-4 border-l-4 border-gray-400"
+          />
+        ),
+        ul: (props) => (
+          <Ul className="list-outside ml-6 space-y-2" {...props} />
+        ),
+        ol: (props) => (
+          <Ol className="list-outside ml-6 space-y-2" {...props} />
+        ),
         li: (props) => <Li {...props} />,
         // table: TODO,---
         // tr: TODO,
         // td: TODO,
         // th: TODO,
         // code: TODO,
-        em: (props) => (
-          <em {...props} className="text-lg font-poppins font-bold" />
+        em: ({ className, ...props }) => (
+          <em
+            {...props}
+            className={`text-lg font-poppins font-bold ${className}`}
+          />
         ),
-        strong: (props) => (
-          <strong {...props} className="text-lg font-poppins font-bold" />
+        strong: ({ className, ...props }) => (
+          <strong
+            {...props}
+            className={`text-lg font-poppins font-bold ${className}`}
+          />
         ),
         delete: (props) => <P {...props} className="line-through" />,
         // inlineCode: TODO,
         // hr: TODO,
-        a: (props) => <A {...props} />,
+        a: ({ href, ...props }) => <A to={href} {...props} />,
       }}
       {...props}
     >
